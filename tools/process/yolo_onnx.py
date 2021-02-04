a23c0681fe8d7143d3512bd05067f1b6a5c66c86
hhaAndroid <1286304229@qq.com>
2021-02-04 09:27:45 +0800

    onnx yolo

--- tools/process/yolo_onnx.py
+++ tools/process/yolo_onnx.py
@@ -65,11 +65,13 @@ if __name__ == '__main__':
     img_name_list = list(filter(lambda f: f.find('json') < 0, img_name_list))
 
     ort_session = onnxruntime.InferenceSession("../../tmp.onnx")
+    cpn_session = onnxruntime.InferenceSession("../../cpn.onnx")
     num_classes=['ok','ng']
 
     for img_path in img_name_list:
         img=cv2.imread(img_path)
         image_copy=img.copy()
+        cpn_img= image_copy/255
         img,para_dict=resize(img,(320,320))
         img=img/255
         img=np.float32(img)
@@ -80,18 +82,56 @@ if __name__ == '__main__':
         lables=ort_outs[1] # nx1
 
         # revert to orig img
-        bboxes=_convert_origshape(bboxes,para_dict)
+        bboxes=_convert_origshape(bboxes,para_dict) # nx4
 
-        # for bbox in bboxes:
-        #     bbox_f = np.array(bbox[:4], np.int32)
-        #     image_copy = cv2.rectangle(image_copy, (bbox_f[0], bbox_f[1]), (bbox_f[2], bbox_f[3]), (0,0,255),
-        #                                5)
-        #
-        # cv2.namedWindow('img',0)
-        # cv2.imshow('img',image_copy)
-        # cv2.waitKey(0)
+        for bbox in bboxes:
+            if np.any(bbox<0):continue
+            bbox_f = np.array(bbox[:4], np.int32)
+            patch=cpn_img[bbox_f[1]:bbox_f[3],bbox_f[0]:bbox_f[2]]
+            img, cpn_para_dict = resize(patch, (320, 320))
+            img1 = np.transpose(img, (2, 0, 1))[None]
+            img1 = np.float32(img1)
+            ort_inputs = {cpn_session.get_inputs()[0].name: img1}
+            array_bcwh = cpn_session.run(None, ort_inputs)[0]  # b,c,h,w
 
-        #
+            h, w = array_bcwh.shape[2], array_bcwh.shape[3]
+            array_bcwh = np.reshape(array_bcwh, [array_bcwh.shape[0], array_bcwh.shape[1], -1])
+            index = np.argmax(array_bcwh, axis=2)
+            y, x = np.unravel_index(index, (h, w))
+            pts_loc_bxcx2 = np.stack([x, y], axis=2).astype(np.float32)
+            pts_bxcx1 = np.max(array_bcwh, axis=2)[..., None]
+
+            pts_loc_cx2 = pts_loc_bxcx2[0, ...]
+            pts_loc_cx2 *= 4
+
+            top = cpn_para_dict['pad_tblr'][0]
+            left = cpn_para_dict['pad_tblr'][2]
+            scale = cpn_para_dict['scale']
+            pts_loc_cx2[..., 0] = pts_loc_cx2[..., 0] - left
+            pts_loc_cx2[..., 1] = pts_loc_cx2[..., 1] - top
+            pts_loc_cx2 = pts_loc_cx2 / scale
+
+            pts_loc_cx2[..., 0] += bbox_f[0]
+            pts_loc_cx2[..., 1] += bbox_f[1]
+
+            pts_loc_cx2 = np.array(pts_loc_cx2, np.int64)
+            for keypoint in pts_loc_cx2:
+                cv2.circle(image_copy, tuple(keypoint), radius=6, color=(0, 0, 255), thickness=-1)
+
+            # cv2.namedWindow('img', 0)
+            # cv2.imshow('img', image_copy)
+            # cv2.waitKey(0)
+
+        for bbox in bboxes:
+            bbox_f = np.array(bbox[:4], np.int32)
+            image_copy = cv2.rectangle(image_copy, (bbox_f[0], bbox_f[1]), (bbox_f[2], bbox_f[3]), (0,0,255),
+                                       5)
+
+        cv2.namedWindow('img',0)
+        cv2.imshow('img',image_copy)
+        cv2.waitKey(0)
+
+
 
 
 
