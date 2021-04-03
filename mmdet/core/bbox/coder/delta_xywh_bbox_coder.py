dba5be9b4eb7975d4b3500cc96b41cb1fc98c443
hha <1286304229@qq.com>
2021-04-03 17:10:05 +0800

    temp

--- mmdet/core/bbox/coder/delta_xywh_bbox_coder.py
+++ mmdet/core/bbox/coder/delta_xywh_bbox_coder.py
@@ -194,9 +194,7 @@ def delta2bbox(rois,
     dy = denorm_deltas[..., 1::4]
     dw = denorm_deltas[..., 2::4]
     dh = denorm_deltas[..., 3::4]
-    max_ratio = np.abs(np.log(wh_ratio_clip))
-    dw = dw.clamp(min=-max_ratio, max=max_ratio)
-    dh = dh.clamp(min=-max_ratio, max=max_ratio)
+
     x1, y1 = rois[..., 0], rois[..., 1]
     x2, y2 = rois[..., 2], rois[..., 3]
     # Compute center of each roi
@@ -205,12 +203,31 @@ def delta2bbox(rois,
     # Compute width/height of each roi
     pw = (x2 - x1).unsqueeze(-1).expand_as(dw)
     ph = (y2 - y1).unsqueeze(-1).expand_as(dh)
+
+    dx_width = pw * dx
+    dy_height = ph * dy
+    max_ratio = np.abs(np.log(wh_ratio_clip))
+
+    if 0:
+        ctr_clamp = 32
+        dx_width = torch.clamp(dx_width,
+                               max=ctr_clamp,
+                               min=-ctr_clamp)
+        dy_height = torch.clamp(dy_height,
+                                max=ctr_clamp,
+                                min=-ctr_clamp)
+        dw = torch.clamp(dw, max=max_ratio)
+        dh = torch.clamp(dh, max=max_ratio)
+    else:
+        dw = dw.clamp(min=-max_ratio, max=max_ratio)
+        dh = dh.clamp(min=-max_ratio, max=max_ratio)
+
     # Use exp(network energy) to enlarge/shrink each roi
     gw = pw * dw.exp()
     gh = ph * dh.exp()
     # Use network energy to shift the center of each roi
-    gx = px + pw * dx
-    gy = py + ph * dy
+    gx = px + dx_width
+    gy = py + dy_height
     # Convert center-xy/width/height to top-left, bottom-right
     x1 = gx - gw * 0.5
     y1 = gy - gh * 0.5
